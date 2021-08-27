5c810c6ec07006f6fda2a4842cd61980430ae377
peppe-99 <g.cardaropoli99@gmail.com>
2021-08-27 13:16:07 +0200

    Aggiunto progetto Permessi

---
+++ 10-Permessi/app/src/main/java/com/example/a10_permessi/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.a10_permessi;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_CODE = 1;
+    private TextView tvPermessoRichiesto;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvPermessoRichiesto = findViewById(R.id.tvPermessoRichiesto);
+        aggiornaTextView();
+    }
+
+    private void aggiornaTextView() {
+
+        //In questo modo controlliamo se l'utente ci ha fornito il permesso della fotocamera
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            tvPermessoRichiesto.setText("Permesso camera: true");
+        }
+        else {
+            tvPermessoRichiesto.setText("Permesso camera: false");
+        }
+    }
+
+    public void richiediPermessoFotocamera(View view) {
+        //Controlliamo se l'utente non ci ha già fornito il permesso
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //Se l'utente non ci ha già fornito il permesso, lo richiediamo cosi
+            //Questo metodo lancia il metodo onRequestPermissionsResult, che andiamo a sovrascrivere
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, //Array dei permessi richiesti
+                    REQUEST_CODE);
+        }
+    }
+
+    @Override //Questo metodo viene lanciato ogni qual volta viene richiesto un permesso e prende come input
+    //il codice della richiesta, i permessi richiesti ed un array di interi che indicherà se il permesso i-esimo è stato dato o meno
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                //Controlliamo se il primo permesso è stato richiesto o meno
+                //Nel nostro caso avremo solamente il permesso della fotocamera
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    aggiornaTextView();
+                } else { //In entrambi i casi invochiamo il metodo aggiornaTextView
+                    aggiornaTextView();
+                }
+            }
+        }
+    }
+}
---
+++ 11-ThreadNO/app/src/main/java/com/example/a11_threadno/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.a11_threadno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    ImageView imgView;
+    TextView tvContatore;
+    Bitmap bmap;
+    private int contatore = 0;
+    private int index = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imgView);
+        tvContatore = findViewById(R.id.tvcontatore);
+    }
+
+    private void onClickCaricaImmagine(View v) {
+        // Simuliamo un caricamento di 5 secondi
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        switch (index) {
+            case 1:
+                bmap = BitmapFactory.decodeResource(getResources(), R.drawable.image1);
+                index++;
+                break;
+            case 2:
+                bmap = BitmapFactory.decodeResource(getResources(), R.drawable.image2);
+                index++;
+                break;
+            case 3:
+                bmap = BitmapFactory.decodeResource(getResources(), R.drawable.image3);
+                index = 1;
+                break;
+        }
+        imgView.setImageBitmap(bmap);
+    }
+
+    // questa funzione non potrà essere invocata durante il finto caricamento dell'immagine
+    // poichè abbiamo un solo thread.
+    private void onClickContatore(View v) {
+        contatore++;
+        tvContatore.setText(contatore);
+    }
+}

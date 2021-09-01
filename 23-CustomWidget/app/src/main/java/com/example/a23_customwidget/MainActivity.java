64858424e3a1848df2676c17855c9e5db15b8a72
peppe-99 <g.cardaropoli99@gmail.com>
2021-09-01 12:59:12 +0200

    Progetto CustomWidget

---
+++ 23-CustomWidget/app/src/main/java/com/example/a23_customwidget/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a23_customwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout container = findViewById(R.id.container);
+
+        CustomWidget cw1, cw2, cw3;
+        cw1 = new CustomWidget(getApplicationContext(),300,100, Color.RED);
+        cw2 = new CustomWidget(getApplicationContext(),200,200, Color.YELLOW);
+        cw3 = new CustomWidget(getApplicationContext(),150,50, Color.BLUE);
+        container.addView(cw1);
+        container.addView(cw2);
+        container.addView(cw3);
+    }
+}
